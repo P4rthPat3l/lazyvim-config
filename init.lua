0adff2a2e76aa933affe43c00d046c3cdd6a0b30
Your Name <140066704+Parth002933v@users.noreply.github.com>
2025-03-12 00:25:17 +0530

    add auto save

--- init.lua
+++ init.lua
@@ -4,3 +4,8 @@ require("config.lazy")
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldenable = true
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*",
+  command = "wa",
+})
